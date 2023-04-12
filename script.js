40457194766d74f80c279ef46c8e520a9c028b08
Student <Brightdylan93@gmail.com>
2023-04-12 17:12:54 -0500

    done

---
+++ script.js
@@ -0,0 +1,34 @@
+// 1. 
+
+for (i=1; i<8; i++){
+    console.log(i);
+}
+
+// 2.
+for (i=5; i < 26; i=i+4) {
+    console.log(i);
+}
+
+
+// 3a.
+const wizards = [`Harry Potter`, `Hermione Granger`, `Ron Weasley`];
+
+
+// 3b.
+for (wiz of wizards) {
+    console.log(wiz);
+}
+
+
+// 4a. 
+let harryPotterMovies = 0;
+
+
+// 4b. 
+while (harryPotterMovies < 8) {
+    harryPotterMovies++;
+}
+
+
+//4c.
+console.log(harryPotterMovies)
